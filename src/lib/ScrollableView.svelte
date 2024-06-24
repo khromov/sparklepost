823e9e4043a3349184a3f78585764064b1c50129
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-06-24 21:37:05 +0200

    WIP

---
+++ src/lib/ScrollableView.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+    import { onMount } from 'svelte';
+
+    let scrollContainer: HTMLElement;
+    let scrollPosition = 0;
+
+    onMount(() => {
+        scrollContainer.scrollTop = scrollPosition;
+    });
+
+    function handleScroll() {
+        scrollPosition = scrollContainer.scrollTop;
+    }
+</script>
+
+<div class="scrollable-content" bind:this={scrollContainer} on:scroll={handleScroll}>
+    <slot />
+</div>
+
+<style>
+    .scrollable-content {
+        height: 100%;
+        overflow-y: auto;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import Message from '$lib/Message.svelte';
+	import ScrollableView from '$lib/ScrollableView.svelte';
 	import { activeTabIndex } from '$lib/stores/tab';
 	import { onMount } from 'svelte';
 	import Swiper from 'swiper/bundle';
 	import 'swiper/css/bundle';
 
 	const tabs = ['For you', 'Following', 'Svelte Society'];
-	let activeIndex = 0;
 	let swiper: Swiper | null;
 	let swiperEl: HTMLElement;
+
+	let mounted = false;
 
 	onMount(() => {
 		swiper = new Swiper(swiperEl, {
@@ -24,31 +26,30 @@
 				}
 			}
 		});
+
+        // To avoid rerenders
+		mounted = true;
 	});
 
-    $: swiper?.slideTo($activeTabIndex);
-
-	function handleTabChange(event: CustomEvent<number>) {
-		if (swiper) {
-			swiper.slideTo(event.detail);
-		}
-	}
+	$: swiper && mounted && swiper?.slideTo($activeTabIndex);
 </script>
 
 <div class="swiper" bind:this={swiperEl}>
 	<div class="swiper-wrapper">
 		{#each tabs as tab}
 			<div class="swiper-slide">
-				{#each Array(10) as _, i}
-					<Message />
-				{/each}
+				<ScrollableView>
+					{#each Array(10) as _, i}
+						<Message />
+					{/each}
+				</ScrollableView>
 			</div>
 		{/each}
 	</div>
 </div>
 
 <style>
-    /*
+	/*
 	:global(.swiper-slide) {
 		height: auto;
 		overflow-y: auto;
