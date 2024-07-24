52be57c10e27c3d9acbbc42aaefabf6bf3df6fc5
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-07-24 02:46:55 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,16 +11,16 @@
 
 		if (!document.startViewTransition) return;
 
-		// console.log('ft', navigation.from?.route.id, navigation.to?.route.id);
+		console.log('ft', navigation.from?.route.id, navigation.to?.route.id);
 
-		//if(navigation.from?.route.id === navigation.to?.route.id) {
-		//	return;
-		//}
+		if(navigation.from?.route.id === navigation.to?.route.id) {
+			return;
+		}
 
 
-		//if (navigation.to?.route.id === '/') {
-		//	document.documentElement.classList.add('back-transition');
-		//}
+		if (navigation.to?.route.id === '/') {
+			document.documentElement.classList.add('back-transition');
+		}
 
 		return new Promise((resolve) => {
 			const transition = document.startViewTransition(async () => {
@@ -28,9 +28,9 @@
 				await navigation.complete;
 			});
 
-			//transition.finished.finally(() => {
-			//	document.documentElement.classList.remove('back-transition');
-			//});
+			transition.finished.finally(() => {
+				document.documentElement.classList.remove('back-transition');
+			});
 		});
 
 	});
