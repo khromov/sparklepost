f4977eaff3a5b5879534dd00741016f4cdc44a62
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-07-25 04:52:02 +0200

    Update ScrollableView.svelte

--- src/lib/ScrollableView.svelte
+++ src/lib/ScrollableView.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-    let scrollContainer: HTMLElement;
-    let scrollPosition = 0;
+	let scrollContainer: HTMLElement;
+	let scrollPosition = 0;
 
-    export let scrollableView: HTMLElement | undefined = undefined;
+	export let scrollableView: HTMLElement | undefined = undefined;
 
-    onMount(() => {
-        scrollContainer.scrollTop = scrollPosition;
-        scrollableView = scrollContainer;
-    });
+	onMount(() => {
+		scrollContainer.scrollTop = scrollPosition;
+		scrollableView = scrollContainer;
+	});
 
-    function handleScroll() {
-        scrollPosition = scrollContainer.scrollTop;
-    }
+	function handleScroll() {
+		scrollPosition = scrollContainer.scrollTop;
+	}
 </script>
 
 <div class="scrollable-content" bind:this={scrollContainer} on:scroll={handleScroll}>
-    <slot />
+	<slot />
 </div>
 
 <style>
-    .scrollable-content {
-        height: calc(100vh - 56px - 54px - 50px); /* subtract top and bottom bars */
-        overflow-y: auto;
-    }
+	.scrollable-content {
+		height: calc(100vh - 56px - 54px - 50px); /* subtract top and bottom bars */
+		overflow-y: auto;
+	}
 </style>
